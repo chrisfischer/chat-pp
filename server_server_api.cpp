f424162e4a1e7ce56e5c235cb428493f9a860d17
Chris Fischer <chris.fischer36@gmail.com>
2020-04-18 19:02:27 -0400

    Server server proto

--- server_server_api.cpp
+++ server_server_api.cpp
@@ -1,6 +1,12 @@
-#include "proto/server_server.pb.h"
+#include <grpcpp/grpcpp.h>
+
+#include "proto/server_server.grpc.pb.h"
 
 class ServerServerApi {
+private:
+    std::unique_ptr<server_server::MessageService::Stub> stub_;
+
 public:
+    ServerServerApi(std::shared_ptr<grpc::Channel> channel);
 
 };
