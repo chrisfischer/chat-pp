3aa8b70f7eff144f914823f974978172a9a0a795
carokun <carokun@sas.upenn.edu>
2020-04-23 02:21:18 -0400

    fixing style

--- src/client.cpp
+++ src/client.cpp
@@ -17,30 +17,42 @@ mutex m;
 void print_help_message()
 {
   cout << "Ĉ++ has five special commands:" << endl;
-  cout << "1. help : Type this command at any time to see this help menu again." << endl;
-  cout << "2. enter <chatroom> : This command is available when you are not a member of a chatroom. Type this command and the users of <chatroom> will vote on whether to let you in. If they vote yes, you will be entered into <chatroom>." << endl;
-  cout << "3. leave : This command is available when you are a member of a chatroom. Type this command to leave the chatroom." << endl;
-  cout << "4. set nickname <new_nickname> : This command is available when you are inside a chatroom and is used to change your nickname inside that chatroom." << endl;
-  cout << "5. kick <nickname> : This command is available when you are inside a chatroom and is used to kick someone out of the chatroom. If no one exists with the nickname <nickname> nothing will happen." << endl << endl;
+  cout << "1. help : Type this command at any time to see this help menu again."
+    << endl;
+  cout << "2. enter <chatroom> : This command is available when you are not a "
+    "member of a chatroom. Type this command and the users of <chatroom> will "
+    "vote on whether to let you in. If they vote yes, you will be entered into "
+    "<chatroom>." << endl;
+  cout << "3. leave : This command is available when you are a member of a "
+    "chatroom. Type this command to leave the chatroom." << endl;
+  cout << "4. set nickname <new_nickname> : This command is available when you "
+    "are inside a chatroom and is used to change your nickname inside that "
+    "chatroom." << endl;
+  cout << "5. kick <nickname> : This command is available when you are inside "
+    "a chatroom and is used to kick someone out of the chatroom. If no one "
+    "exists with the nickname <nickname> nothing will happen." << endl << endl;
 }
 
 string prompt_cli()
 {
   cout << "Welcome to Ĉ++!" << endl << endl;
   print_help_message();
-  cout << "In order to get started, please specify the IP address you would like to connect to." << endl << "IP Address:" << endl;
+  cout << "In order to get started, please specify the IP address you would "
+    "like to connect to." << endl << "IP Address:" << endl;
 
   // localhost:50051
   string serverIP;
   getline(cin, serverIP);
 
-  cout << "You're all set! You can enter a chatroom now by typing enter <chatroom>." << endl;
+  cout << "You're all set! You can enter a chatroom now by typing enter "
+    "<chatroom>." << endl;
   return serverIP;
 }
 
 void listen_to_server(ClientServerAPI& csAPI) {
   client_server::Message msg;
-  shared_ptr<grpc::ClientReader<client_server::Message>> reader {csAPI.get_reader()};
+  shared_ptr<grpc::ClientReader<client_server::Message>> reader
+    {csAPI.get_reader()};
 
   while (reader->Read(&msg)) {
     if (msg.has_start_vote_message()) {
@@ -63,9 +75,11 @@ void parse_input(string &input, ClientServerAPI& csAPI) {
   } else if (input.rfind("enter", 0) == 0 && !csAPI.in_room()) {
     string room = input.erase(0, 6);
     csAPI.join_room(room);
-    cout << "Users from " << room << " will now vote on whether to let you in the room." << endl;
+    cout << "Users from " << room << " will now vote on whether to let you in "
+      "the room." << endl;
   } else if (!csAPI.in_room()) {
-    cout << "You are not currently in a chatroom. Type enter <chatroom> to enter a room." << endl;
+    cout << "You are not currently in a chatroom. Type enter <chatroom> to "
+      "enter a room." << endl;
   } else if (input.rfind("leave", 0) == 0) {
     csAPI.leave_room();
   } else if (input.rfind("set nickname", 0) == 0) {
@@ -87,7 +101,8 @@ void listen_to_user(ClientServerAPI& csAPI) {
 void run_client() {
   string addr {prompt_cli()};
 
-  ClientServerAPI csAPI {grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials())};
+  ClientServerAPI csAPI {
+    grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials())};
 
   thread serverThread {listen_to_server, ref(csAPI)};
   thread userThread {listen_to_user, ref(csAPI)};
